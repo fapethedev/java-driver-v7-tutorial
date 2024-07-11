944284d30803116047ff93d04962a932b927cc2e
fapethedev <mpetrivlin@gmail.com>
2024-07-11 19:11:07 +0000

    ArangoDB Java Driver v7 Tutorial:  Connect or create a database

--- src/main/java/com/fapethedev/arango/App.java
+++ src/main/java/com/fapethedev/arango/App.java
@@ -2,6 +2,7 @@ package com.fapethedev.arango;
 
 
 import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDatabase;
 
 public class App
 {
@@ -10,5 +11,10 @@ public class App
         ArangoDB arangoDB = new ArangoDB.Builder()
         .host("localhost", 8529)
         .build();
+
+//      Let’s connect to or create a new database:
+        ArangoDatabase db = arangoDB.db("javadriverv7tutoraildb");
+        System.out.println("Creating database...");
+        db.create(); // Not possible if using the generated University database
     }
 }
