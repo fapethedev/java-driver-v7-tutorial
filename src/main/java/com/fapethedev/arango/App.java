f7f752d824985cad8d75562ff121ab6dc834d50c
fapethedev <mpetrivlin@gmail.com>
2024-07-11 19:08:08 +0000

    ArangoDB Java Driver v7 Tutorial:  Connect to ArangoDB

--- src/main/java/com/fapethedev/arango/App.java
+++ src/main/java/com/fapethedev/arango/App.java
@@ -1,10 +1,14 @@
 package com.fapethedev.arango;
 
 
+import com.arangodb.ArangoDB;
+
 public class App
 {
     public static void main( String[] args )
     {
-
+        ArangoDB arangoDB = new ArangoDB.Builder()
+        .host("localhost", 8529)
+        .build();
     }
 }
